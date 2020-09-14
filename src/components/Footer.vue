1fd4bfde0735f0516774916c83f53a5172a93c7f
Emily Chen <wanchichen88@gmail.com>
2020-09-14 15:49:11 +0800

    增加後台連結

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,16 +6,20 @@
                     <ul class="d-flex list-unstyled px-3 mb-0">
                         <li class="">© Copright 2020 Emily Chen</li>
                         <li class="ml-auto px-3">
-                            <a href="#" class="text-primary-dark h4 mb-0">
-                                <span class="  icon-envelope"></span>
+                            <a href="https://github.com/wanchii/a_little" class="text-primary-dark h4 mb-0" target="_blank">
+                                <span class="icon-envelope"></span>
                             </a>
                         </li>
-                        <!-- <li class="px-3">
-                            <a href="#" class="text-primary-dark h4 mb-0">
-                                <span class="icon-facebook"></span>
-                            </a>
+                        <li class="px-3">
+                            <!-- <a href="#" class="text-primary-dark h4 mb-0">
+                                <span class="icon-gears"></span>
+                            </a> -->
+                            <router-link class="text-primary-dark h4 mb-0"
+                                to="/login">
+                                <span class="icon-gears"></span>
+                            </router-link>
                         </li>
-                        <li class="pl-3">
+                        <!-- <li class="pl-3">
                             <a href="#" class="text-primary-dark h4 mb-0">
                                 <span class="icon-twitter"></span>
                             </a>
