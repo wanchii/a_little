0113045da1187c85c38de14160235a7e644646ec
Emily Chen <wanchichen88@gmail.com>
2020-09-17 00:27:54 +0800

    食譜

--- src/views/front/Recipe.vue
+++ src/views/front/Recipe.vue
@@ -2,19 +2,108 @@
 <section>
     <div class="container">
       <div class="row py-5 mb-5">
-        <div class="col-md-6">
-          <img src="https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
-            alt="">
+        <div class="col-md-6 bg-cover"
+          style="
+          background-image:url(https://images.unsplash.com/photo-1552845683-cfefc701e423?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60);
+          width:540px;height:360px;">
         </div>
         <div class="col-md-6">
-          <div class="p-6 d-flex align-items-center h-100">
-            <p class="h5 mb-3 lh-2">
-              <span>待更新...</span>
-            </p>
+          <div class="p-6 d-flex flex-column align-items-center h-100">
+            <h3 class="font-weight-bold text-primary-dark">風琴馬鈴薯</h3>
+            <small class="text-primary">烤好的馬鈴薯就像風琴一樣綻放</small>
+            <div class="mb-3 lh-2">
+              <p>食材：馬鈴薯
+                <br>
+                調味料：橄欖油、無鹽奶油、鹽、黑胡椒、新鮮迷迭香
+              </p>
+              <p>先將馬鈴薯外皮刷洗乾淨，兩側擺上筷子協助切出刀痕不斷的馬鈴薯，寬度隨意。
+                用工具將每個切面均勻抹上橄欖油，灑點海鹽及黑胡椒。</p>
+              <p>烤盤放上烤盤紙後，擺上馬鈴薯，可在烤盤四周再加點奶油，上方放置新鮮迷迭香
+                烤箱事先預熱180℃，再將馬鈴薯入烤箱烤約20-40分鐘
+              </p>
+            </div>
           </div>
         </div>
       </div>
-
+      <div class="row flex-row-reverse py-5 mb-5">
+        <div class="col-md-6 bg-cover"
+          style="
+          background-image:url(https://images.unsplash.com/photo-1516295615676-7ae4303c1c63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);
+          width:540px;height:360px;">
+        </div>
+        <div class="col-md-6">
+          <div class="p-6 d-flex flex-column align-items-center h-100">
+            <h3 class="font-weight-bold text-primary-dark">蜂蜜肉桂烤梨</h3>
+            <small class="text-primary">肉桂的香氣，搭配蜂蜜的甜</small>
+            <div class="mb-3 lh-2">
+              <p>食材：西洋梨
+                <br>
+                調味料：肉桂、蜂蜜
+              </p>
+              <p>先將馬鈴薯外皮刷洗乾淨，兩側擺上筷子協助切出刀痕不斷的馬鈴薯，寬度隨意。
+                用工具將每個切面均勻抹上橄欖油，灑點海鹽及黑胡椒。</p>
+              <p>烤盤放上烤盤紙後，擺上馬鈴薯，可在烤盤四周再加點奶油，上方放置新鮮迷迭香
+                烤箱事先預熱180℃，再將馬鈴薯入烤箱烤約20-40分鐘
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row py-5 mb-5">
+        <div class="col-md-6 bg-cover"
+          style="
+          background-image:url(https://images.unsplash.com/photo-1593629718888-9009c55e6cad?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60);
+          width:540px;height:360px;">
+        </div>
+        <div class="col-md-6">
+          <div class="p-6 d-flex flex-column align-items-center h-100">
+            <h3 class="font-weight-bold text-primary-dark">佛卡夏</h3>
+            <small class="text-primary">麵包與橄欖油搭配，絕妙</small>
+            <div class="mb-3 lh-2">
+              <p>食材：西洋梨
+                <br>
+                調味料：肉桂、蜂蜜
+              </p>
+              <p>先將馬鈴薯外皮刷洗乾淨，兩側擺上筷子協助切出刀痕不斷的馬鈴薯，寬度隨意。
+                用工具將每個切面均勻抹上橄欖油，灑點海鹽及黑胡椒。</p>
+              <p>烤盤放上烤盤紙後，擺上馬鈴薯，可在烤盤四周再加點奶油，上方放置新鮮迷迭香
+                烤箱事先預熱180℃，再將馬鈴薯入烤箱烤約20-40分鐘
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row flex-row-reverse py-5 mb-5">
+        <div class="col-md-6 bg-cover"
+          style="
+          background-image:url(https://images.pexels.com/photos/1666061/pexels-photo-1666061.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500);
+          width:540px;height:360px;">
+        </div>
+        <div class="col-md-6">
+          <div class="p-6 d-flex flex-column align-items-center h-100">
+            <h3 class="font-weight-bold text-primary-dark">香料紅酒</h3>
+            <small class="text-primary mb-2">溫暖的紅酒，從胃開始暖呼呼</small>
+            <div class="mb-3 lh-2">
+              <p>食材：柳丁、檸檬、便宜紅酒
+                <br>
+                調味料：丁香、八角、肉桂棒、砂糖、黑胡椒粒
+              </p>
+              <p>將紅酒、糖、肉桂棒、丁香、八角、黑胡椒粒、切片檸檬、切片柳丁一起煮
+                將近沸騰的起泡狀態熄火，倒入柳丁汁與檸檬汁，過濾後就可以飲用。
+                鍋中的肉桂棒也可以取出浸泡在煮好的熱紅酒中增加肉桂風味
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </section>
 </template>
+<style lang="scss" scoped>
+.recipe-img{
+  width: 540px;
+  height: 360px;
+  border-radius: 0px;
+}
+
+</style>
