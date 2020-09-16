df1a0d21fba0dafc58c991812f5483a14c17c8f0
Emily Chen <wanchichen88@gmail.com>
2020-09-16 22:50:43 +0800

    rwd

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,27 +3,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <ul class="d-flex list-unstyled px-3 mb-0">
-                        <li class="">© Copright 2020 Emily Chen</li>
+                    <ul class="d-flex list-unstyled px-3 mb-0 flex-md-row flex-column">
+                        <li class="mr-2">© Copright 2020 Emily Chen</li>
+                        <li class="mr-2">網站素材都取自網路，僅為練習之用</li>
                         <li class="ml-auto px-3">
                             <a href="https://github.com/wanchii/a_little" class="text-primary-dark h4 mb-0" target="_blank">
                                 <span class="icon-envelope"></span>
                             </a>
-                        </li>
-                        <li class="px-3">
-                            <!-- <a href="#" class="text-primary-dark h4 mb-0">
-                                <span class="icon-gears"></span>
-                            </a> -->
-                            <router-link class="text-primary-dark h4 mb-0"
+                            <router-link class="text-primary-dark h4 mb-0 ml-3"
                                 to="/login">
                                 <span class="icon-gears"></span>
                             </router-link>
                         </li>
-                        <!-- <li class="pl-3">
-                            <a href="#" class="text-primary-dark h4 mb-0">
-                                <span class="icon-twitter"></span>
-                            </a>
-                        </li> -->
+
                     </ul>
                 </div>
             </div>
