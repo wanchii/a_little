9913a31d7cbdcb491e1f6353b91321bac26b7d8f
Emily Chen <wanchichen88@gmail.com>
2020-09-18 16:03:15 +0800

    aos效果

--- src/views/front/Recipe.vue
+++ src/views/front/Recipe.vue
@@ -1,14 +1,16 @@
 <template>
-<section>
+<section class="mt-5">
     <div class="container">
-      <div class="row py-5 mb-5">
+      <div
+      data-aos="fade-left" data-aos-duration="1000"
+      class="row mb-5 bg-light">
         <div class="col-md-6 bg-cover"
           style="
           background-image:url(https://images.unsplash.com/photo-1552845683-cfefc701e423?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60);
           width:540px;height:360px;">
         </div>
         <div class="col-md-6">
-          <div class="p-6 d-flex flex-column align-items-center h-100">
+          <div class="px-6 d-flex flex-column justify-content-center align-items-center h-100">
             <h3 class="font-weight-bold text-primary-dark">風琴馬鈴薯</h3>
             <small class="text-primary">烤好的馬鈴薯就像風琴一樣綻放</small>
             <div class="mb-3 lh-2">
@@ -25,14 +27,16 @@
           </div>
         </div>
       </div>
-      <div class="row flex-row-reverse py-5 mb-5">
+      <div
+      data-aos="fade-right" data-aos-duration="1000"
+      class="row flex-row-reverse mb-5 bg-gray-200">
         <div class="col-md-6 bg-cover"
           style="
           background-image:url(https://images.unsplash.com/photo-1516295615676-7ae4303c1c63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80);
           width:540px;height:360px;">
         </div>
         <div class="col-md-6">
-          <div class="p-6 d-flex flex-column align-items-center h-100">
+          <div class="px-6 d-flex flex-column justify-content-center align-items-center h-100">
             <h3 class="font-weight-bold text-primary-dark">蜂蜜肉桂烤梨</h3>
             <small class="text-primary">肉桂的香氣，搭配蜂蜜的甜</small>
             <div class="mb-3 lh-2">
@@ -49,14 +53,16 @@
           </div>
         </div>
       </div>
-      <div class="row py-5 mb-5">
+      <div
+      data-aos="fade-left" data-aos-duration="1000"
+      class="row mb-5">
         <div class="col-md-6 bg-cover"
           style="
           background-image:url(https://images.unsplash.com/photo-1593629718888-9009c55e6cad?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60);
           width:540px;height:360px;">
         </div>
         <div class="col-md-6">
-          <div class="p-6 d-flex flex-column align-items-center h-100">
+          <div class="px-6 d-flex flex-column justify-content-center align-items-center h-100">
             <h3 class="font-weight-bold text-primary-dark">佛卡夏</h3>
             <small class="text-primary">麵包與橄欖油搭配，絕妙</small>
             <div class="mb-3 lh-2">
@@ -73,14 +79,16 @@
           </div>
         </div>
       </div>
-      <div class="row flex-row-reverse py-5 mb-5">
+      <div
+      data-aos="fade-right" data-aos-duration="1000"
+      class="row flex-row-reverse mb-5 bg-base">
         <div class="col-md-6 bg-cover"
           style="
           background-image:url(https://images.pexels.com/photos/1666061/pexels-photo-1666061.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500);
           width:540px;height:360px;">
         </div>
         <div class="col-md-6">
-          <div class="p-6 d-flex flex-column align-items-center h-100">
+          <div class="px-6 d-flex flex-column justify-content-center align-items-center h-100">
             <h3 class="font-weight-bold text-primary-dark">香料紅酒</h3>
             <small class="text-primary mb-2">溫暖的紅酒，從胃開始暖呼呼</small>
             <div class="mb-3 lh-2">
